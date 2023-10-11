7399f99a910c0443eaef2a99611b8d002fd1eb33
MarksonArguello <marksonarguello@gmail.com>
2023-10-11 15:25:17 -0300

    Refactoring folders and Each Choice

--- trab2/tests/test_batida_ec.py
+++ trab2/tests/test_batida_ec.py
@@ -1,5 +1,11 @@
-from batida import colisao
+# Casos de Testes: Each Choice
 
+import sys
+sys.path.append('../')
+
+from src.batida import colisao
+
+# Caso 1: Retângulos iguais separados na mesma altura
 def test_retangulos_iguais_separados_mesma_altura():
   # Retângulo 1
   x1 = 0
@@ -19,7 +25,7 @@ def test_retangulos_iguais_separados_mesma_altura():
   
   assert retorno == valor_esperado
 
-
+# Caso 2: Retângulos iguais separados em alturas diferentes
 def test_retangulos_iguais_separados_alturas_diferentes():
   # Retângulo 1
   x1 = 3
@@ -39,7 +45,7 @@ def test_retangulos_iguais_separados_alturas_diferentes():
   
   assert retorno == valor_esperado
 
-
+# Caso 3: Retângulos iguais colados um em cima do outro
 def test_retangulos_iguais_colados_um_em_cima_do_outro():
   # Retângulo 1
   x1 = 0
@@ -59,6 +65,7 @@ def test_retangulos_iguais_colados_um_em_cima_do_outro():
   
   assert retorno == valor_esperado
 
+# Caso 4: Retângulos iguais com intersecção em um ponto
 def test_retangulos_iguais_interseccao_em_um_ponto():
   # Retângulo 1
   x1 = 2
@@ -78,6 +85,7 @@ def test_retangulos_iguais_interseccao_em_um_ponto():
   
   assert retorno == valor_esperado
 
+# Caso 5: Retângulos iguais com um lado na mesma vertical
 def test_retangulos_iguais_com_um_lado_mesma_vertical():
   # Retângulo 1
   x1 = 0
